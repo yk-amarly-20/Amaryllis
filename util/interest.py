4f47637e87c6663b17e677b0227d327ff00409d0
小島悠人 <kojimayuto@mbp.local>
2020-04-05 21:31:54 +0900

    計算基数を実装しました

---
+++ util/interest.py
@@ -0,0 +1,108 @@
+# 利息計算を行う
+import numpy as np
+
+
+def present_value_rate(i):
+    """
+    現価率を計算
+
+    Parameters
+    ----------
+    i: float
+        年利率
+
+    Returns
+    -------
+    v: float
+        現価率
+    """
+
+    v = 1 / (1 + i)
+
+    return v
+
+
+def discount_rate(i):
+    """
+    割引率を計算
+
+    Parameters
+    ----------
+    i: float
+        年利率
+
+    Returns
+    -------
+    d: float
+        割引率
+    """
+
+    d = i / (1 + i)
+
+    return d
+
+
+def name_interest_rate(i, k):
+    """
+    名称利率を計算
+
+    Parameters
+    ----------
+    i: float
+        年利率
+    k: int
+        一年に支払う回数
+
+    Returns
+    -------
+    i_k: float
+        年k回払いの名称利率
+    """
+
+    i_k = k * ((1 + i) ** (1 / k) - 1)
+
+    return i_k
+
+
+def name_discount_rate(i, k):
+    """
+    名称割引率を計算
+
+    Parameters
+    ----------
+    i:　float
+        年利率
+    k: int
+        一年に支払う回数
+
+    Returns
+    -------
+    d_k: float
+        年k回払いの名称割引率
+    """
+
+    i_k = name_interest_rate(i, k)
+
+    d_k = i_k / (1 + i_k / k)
+
+    return d_k
+
+
+def force_of_interest(i):
+    """
+    利力を計算
+
+    Parameters
+    ----------
+    i: float
+        年利率
+
+    Returns
+    -------
+    delta: float
+        利力
+    """
+
+    delta = np.log(1 + i)
+
+    return delta
