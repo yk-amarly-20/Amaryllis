de38cea32a74448392c4f15fb47fc0f08aeebcef
小島悠人 <kojimayuto@mbp.local>
2020-04-05 21:30:14 +0900

    テストケースを追加しました

---
+++ tests/util/calc_base_test.py
@@ -0,0 +1,72 @@
+# calc_base.pyのテストケース
+
+import sys
+sys.path.append('./../../')
+from util.calc_base import *
+import numpy as np
+
+
+POPULATION = np.array([1188, 774, 487, 295, 172, 96, 51, 26, 13, 6, 2, 1])
+DEAD_POPULATION = np.array([414, 287, 192, 123, 76, 45, 25, 13, 7, 3, 1, 1])
+I = 0.03
+LIFESPAN = 110
+X = 100
+
+
+def test_D():
+    D(I, POPULATION[0], X)
+
+    return
+
+
+def test_N():
+    N(I, POPULATION, X, LIFESPAN)
+
+    return
+
+
+def test_C():
+    C(I, DEAD_POPULATION[0], X)
+
+    return
+
+
+def test_M():
+    M(I, DEAD_POPULATION, X, LIFESPAN)
+
+    return
+
+
+def test_S():
+    S(I, POPULATION, X, LIFESPAN)
+
+    return
+
+
+def test_R():
+    R(I, DEAD_POPULATION, X, LIFESPAN)
+
+    return
+
+
+def test_C_continuous():
+    C_continuous(I, DEAD_POPULATION[0], LIFESPAN)
+
+    return
+
+
+def test_M_continuous():
+    M_continuous(I, DEAD_POPULATION, X, LIFESPAN)
+
+    return
+
+
+if __name__ == '__main__':
+    test_D()
+    test_N()
+    test_C()
+    test_M()
+    test_S()
+    test_R()
+    test_C_continuous()
+    test_M_continuous()
---
+++ tests/util/interest_test.py
@@ -0,0 +1,48 @@
+# interest.pyのテストケース
+
+import numpy as np
+import sys
+sys.path.append('./../../')
+from util.interest import *
+
+
+I = 0.03
+K = 4
+
+
+def test_present_value_rate():
+    present_value_rate(I)
+
+    return
+
+
+def test_discount_rate():
+    discount_rate(I)
+
+    return
+
+
+def test_name_interest_rate():
+    name_discount_rate(I, K)
+
+    return
+
+
+def test_name_discount_rate():
+    name_discount_rate(I, K)
+
+    return
+
+
+def test_force_of_interest():
+    force_of_interest(I)
+
+    return
+
+
+if __name__ == '__main__':
+    test_present_value_rate()
+    test_discount_rate()
+    test_name_interest_rate()
+    test_name_discount_rate()
+    test_force_of_interest()
